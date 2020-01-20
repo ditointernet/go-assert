9e13125a7018cefcea577ce38c0d90dea82a89b5
Vinícius Garcia <vingarcia00@gmail.com>
2020-01-20 13:43:40 -0300

    Add NotEqual function

--- assert.go
+++ assert.go
@@ -6,8 +6,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// AssertEq compares with a deep equal if two values are the same
+// Equal compares with a deep equal if two values are the same
 // if not it calls t.Fatalf() with a readable diff and returns.
 func Equal(t *testing.T, expected interface{}, received interface{}, desc ...interface{}) {
 	require.Equal(t, expected, received, desc)
+}
+
+// NotEqual compares with a deep equal if two values are the same
+// if not it calls t.Fatalf() with a readable diff and returns.
+func NotEqual(t *testing.T, expected interface{}, received interface{}, desc ...interface{}) {
+	require.NotEqual(t, expected, received, desc)
 }
--- assert_test.go
+++ assert_test.go
@@ -19,3 +19,16 @@ func TestEqual(t *testing.T) {
 		Equal(t, struct{ a int }{10}, struct{ a int }{11}, "Error message")
 	})
 }
+
+func TestNotEqual(t *testing.T) {
+	t.Run("call t.Fatal() on error", func(t *testing.T) {
+		fmt.Println("Will always print this")
+		NotEqual(t, 10, 10, "Error message")
+
+		fmt.Println("This print will never run")
+	})
+
+	t.Run("works for any type", func(t *testing.T) {
+		NotEqual(t, struct{ a int }{10}, struct{ a int }{10}, "Error message")
+	})
+}
